5253d8d123c649e7155c875361317e3a6acb968c
gamzeguzleobuz <gamzeguzle@gmail.com>
2023-09-14 12:30:51 +0300

    identityseed, constants

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -52,6 +52,13 @@ using (var scope = app.Services.CreateScope())
 { 
  var watchHubContext = scope.ServiceProvider.GetRequiredService<WatchHubContext>();
     await WatchHubContextSeed.SeedAsync(watchHubContext);
+
+
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+    await AppIdentityDbContextSeed.SeedAsync(roleManager, userManager);
 }
 
 
