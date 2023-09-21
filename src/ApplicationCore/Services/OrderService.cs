c5d138be245faa88d7652b936306662122f970d2
gamzeguzleobuz <gamzeguzle@gmail.com>
2023-09-21 10:47:03 +0300

    order service

---
+++ src/ApplicationCore/Services/OrderService.cs
@@ -0,0 +1,44 @@
+﻿using ApplicationCore.Entities;
+using ApplicationCore.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Services
+{
+    public class OrderService : IOrderService
+    {
+        public IBasketService _basketService { get; }
+        public IRepository<Order> _orderRepo { get; }
+
+        public OrderService(IBasketService basketService, IRepository<Order> orderRepo)
+        {
+            _basketService = basketService;
+            _orderRepo = orderRepo;
+        }
+
+
+
+        public async Task<Order> CreateOrderAsync(string buyerId, Address shippingAddress)
+        {
+            var basket = await _basketService.GetOrCreateBasketAsync(buyerId);
+
+            Order order = new Order()
+            {
+                ShipToAddress = shippingAddress,
+                BuyerId = buyerId,
+                Items = basket.Items.Select(x => new OrderItem()
+                {
+                    ProductId = x.ProductId,
+                    ProductName = x.Product.Name,
+                    UnitPrice = x.Product.Price,
+                    PictureUri = x.Product.PictureUri,
+                    Quantity = x.Quantity
+                }).ToList()
+            };
+            return await _orderRepo.AddAsync(order);
+        }
+    }
+}
